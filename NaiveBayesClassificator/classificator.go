451379754d25fb097acaf23b8bec4d799bfb095f
RadostinaIvanova <radi.e.ivanova@gmail.com>
2021-01-14 01:24:24 +0200

    add functions

--- NaiveBayesClassificator/classificator.go
+++ NaiveBayesClassificator/classificator.go
@@ -24,6 +24,7 @@ func extractVocabulary(document []string){
 // }
 //fmt.Println(calNumDocsOfClasses(classes))
 
+//returns number of all docs in all classes
 func calNumAllDocs(classes map[int] []string) int{
 	numOfDocs := 0
 	for _,value := range classes{
@@ -32,18 +33,22 @@ func calNumAllDocs(classes map[int] []string) int{
 	return numOfDocs
 }
 
+//returns number of classes 
 func calNumOfClasses(classes map[int] []string) int{
 	return len(classes)
 }
 
+//returns a slice of strings by spliting a given document into terms
 func extractTerms(doc string) []string{
 	return strings.Fields(doc)
 }
 
+//returns number of documents in each class
 func classDocsNum(classDocs []string) int{
 	return len(classDocs)
 }
 
+//returns a slice of number of documents in each class
 func makeArrayOfNumDocsInClass(classes map[int] []string) []int{
 	arrOfNumDocs := []int{}
 	for key,docs := range classes{
@@ -52,6 +57,8 @@ func makeArrayOfNumDocsInClass(classes map[int] []string) []int{
 	return arrOfNumDocs
 }
 
+//returns slice of probabilties of each class with the formula - 
+//count of documents in class divided by all documents in all classes
 func makeArrPriorC(numOfAllDocs int, arrNumDocsInClass []int) []float64{
 	arr := []float64{}
 	for i ,docsCount := range arrNumDocsInClass {
@@ -60,6 +67,8 @@ func makeArrPriorC(numOfAllDocs int, arrNumDocsInClass []int) []float64{
 	return arr
 }
 
+//returns a slice which each index matches the term in vocabulary of the same index 
+// and its value is the number of counts of the term in all documents 
 func makeArrTermCountInClass(numOfClasses int,vocabulary map [string] []int) []int{
 	termCountArr := []int{}
 	for _, value := range vocabulary{
@@ -70,6 +79,10 @@ func makeArrTermCountInClass(numOfClasses int,vocabulary map [string] []int) []i
 	return termCountArr
 }
 
+//the function receives as arguments a vocabulary and a slice with number of terms in each class
+//and returns map with key string and value slice of floats
+//the keys represent a term from vocabulary and the slice of floats has the values of the cond probability 
+//inside the innermost cycle is the the making of the slice which we assign to the every term of the vocabulary
 func makeArrCondProb(vocabulary map [string] []int, arrNumOfTermClass []int) map [string] []float64{
 	arrCondProb := map [string] []float64{}
 	temp := [] float64{}
@@ -82,9 +95,10 @@ func makeArrCondProb(vocabulary map [string] []int, arrNumOfTermClass []int) map
 	}
 	return arrCondProb
 }
-func TrainMultinomialNB(classes map[int] []string) (map [string] []int,map [string] []float64, []float64 ){
-	numOfAllDocs := calNumAllDocs(classes)
-	numOfClasses := calNumOfClasses(classes)
+
+//returns vocabulary of type map[string] []int where the key is a term 
+//and the slice contains for each class the term frequency
+func makeVocabulary(classes map[int] []string, numOfClasses int) map [string] []int{
 	vocabulary := map [string] []int{}
 	for class, docs := range classes{
 		for _, doc := range docs{
@@ -100,6 +114,13 @@ func TrainMultinomialNB(classes map[int] []string) (map [string] []int,map [stri
 			}
 		}
 	}
+	return vocabulary
+}
+//function trains Multionomal Naive Bayes Classificator and returns vocabulary, priorC and cond probabilty
+func TrainMultinomialNB(classes map[int] []string) (map [string] []int,map [string] []float64, []float64 ){
+	numOfAllDocs := calNumAllDocs(classes)
+	numOfClasses := calNumOfClasses(classes)
+	vocabulary := makeVocabulary(classes,numOfClasses)
 	arrNumDocsInClass := makeArrayOfNumDocsInClass(classes)
 	arrPriorC := makeArrPriorC(numOfAllDocs, arrNumDocsInClass)
 	arrTermCountClass := makeArrTermCountInClass(numOfClasses, vocabulary)
