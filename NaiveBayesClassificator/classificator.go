06e278ad18abe2527a61dc69b416c236cc964f3c
RadostinaIvanova <radi.e.ivanova@gmail.com>
2021-01-16 00:36:38 +0200

    many changes

--- NaiveBayesClassificator/classificator.go
+++ NaiveBayesClassificator/classificator.go
@@ -1,4 +1,4 @@
-package main
+package classificator 
 
 import (
 	 "strings"
@@ -7,17 +7,14 @@ import (
 )
 
 
-func extractVocabulary(document []string){}
-
 func convertText(document []string){}
-
 
 //function trains Multionomal Naive Bayes Classificator and returns vocabulary, priorC and cond probabilty
 func TrainMultinomialNB(classes map[int] []string) (map [string] []int,map [string] []float64, []float64 ){
 	numOfAllDocs := calNumAllDocs(classes)
 	numOfClasses := calNumOfClasses(classes)
 	vocabulary := makeVocabulary(classes,numOfClasses)
-	makeSliceOfNumDocsInClass := makeSliceOfNumDocsInClass(classes)
+	makeSliceOfNumDocsInClass := makeSliceOfNumDocsInClass(classes, numOfClasses)
 	slicePriorC := makeSlicePriorC(numOfAllDocs, makeSliceOfNumDocsInClass)
 	sliceTermCountClass := makeSliceTermCountInClass(numOfClasses, vocabulary)
 	sliceCondProb := makeSliceCondProb(vocabulary,sliceTermCountClass)
@@ -86,105 +83,106 @@ func classDocsNum(classDocs []string) int{
 }
 
 //returns a slice of number of documents in each class
-func makeSliceOfNumDocsInClass(classes map[int] []string) []int{
+func makeSliceOfNumDocsInClass(classes map[int] []string, numOfClasses int) []int{
 	arrOfNumDocs := []int{}
-	for key,docs := range classes{
-		arrOfNumDocs[key] = classDocsNum(docs)
+	for classInd := 0; classInd < len(classes);classInd++ {
+		arrOfNumDocs = append(arrOfNumDocs, classDocsNum(classes[classInd]))
 	}
 	return arrOfNumDocs
-}
-
-//returns slice of probabilties of each class with the formula - 
-//count of documents in class divided by all documents in all classes
-func makeSlicePriorC(numOfAllDocs int, makeSliceOfNumDocsInClass []int) []float64{
+ }
+ 
+ //returns slice of probabilties of each class with the formula - 
+ //count of documents in class divided by all documents in all classes
+ func makeSlicePriorC(numOfAllDocs int, sliceOfNumDocsInClass []int) []float64{
 	arr := []float64{}
-
-	for i ,docsCount := range makeSliceOfNumDocsInClass {
-		arr[i] = float64(docsCount)/ float64(numOfAllDocs)
+ 
+	for _ ,docsCount := range sliceOfNumDocsInClass {
+		arr = append(arr, float64(docsCount)/ float64(numOfAllDocs))
 	}
-
 	return arr
-}
-
-//returns a slice which each index matches the term in vocabulary of the same index 
-// and its value is the number of counts of the term in all documents 
-func makeSliceTermCountInClass(numOfClasses int,vocabulary map [string] []int) []int{
+ }
+ 
+ //returns a slice which each index matches the term in vocabulary of the same index 
+ // and its value is the number of counts of the term in all documents 
+ func makeSliceTermCountInClass(numOfClasses int,vocabulary map [string] []int) []int{
 	termCountArr := []int{}
-
 	for _, value := range vocabulary{
-		for i:=0;i <= numOfClasses; i++ {
-			termCountArr[i] = value[i]
+		for classInd:=0;classInd < numOfClasses; classInd++ {
+			if len(termCountArr) >= numOfClasses{
+				 termCountArr[classInd] += value[classInd] 
+			}else{
+				termCountArr = append(termCountArr,value[classInd])
+			}
 		}
 	}
-
+ 
 	return termCountArr
-}
-
-//the function receives as arguments a vocabulary and a slice with number of terms in each class
-//and returns map with key string and value slice of floats
-//the keys represent a term from vocabulary and the slice of floats has the values of the cond probability 
-//inside the innermost cycle is the the making of the slice which we assign to the every term of the vocabulary
-func makeSliceCondProb(vocabulary map [string] []int, sliceNumOfTermClass []int) map [string] []float64{
-	sliceCondProb := map [string] []float64{}
+ }
+ 
+ //the function receives as arguments a vocabulary and a slice with number of terms in each class
+ //and returns map with key string and value slice of floats
+ //the keys represent a term from vocabulary and the slice of floats has the values of the cond probability 
+ //inside the innermost cycle is the the making of the slice which we assign to the every term of the vocabulary
+ func makeSliceCondProb(vocabulary map [string] []int, sliceNumOfTermClass []int) map [string] []float64{
+	sliceCondProb := make(map [string] []float64)
 	temp := [] float64{}
+   // i := 0
 	sizeV := len(vocabulary)
-
-	for term, _ := range vocabulary{
-		for class,numOfTermInClass := range sliceNumOfTermClass{
-			temp[class] = float64(term[class] + 1)/float64(numOfTermInClass + sizeV)
+	for term, value := range vocabulary{
+		for class,numOfTermsInClass := range sliceNumOfTermClass{
+			temp = append(temp, (float64(value[class] + 1)/float64(numOfTermsInClass + sizeV)))		   
 		}
 		sliceCondProb[term] = temp
-	}
-
+	 }
 	return sliceCondProb
-}
-
-//returns vocabulary of type map[string] []int where the key is a term 
-//and the slice contains for each class the term frequency
-func makeVocabulary(classes map[int] []string, numOfClasses int) map [string] []int{
-	
-	vocabulary := map [string] []int{}
-
+ }
+ 
+ //returns vocabulary of type map[string] []int where the key is a term 
+ //and the slice contains for each class the term frequency
+ func makeVocabulary(classes map[int] []string, numOfClasses int) map [string] []int{
+	vocabulary := make(map [string] []int)
+ 
 	for class, docs := range classes{
 		for _, doc := range docs{
 			terms := extractTerms(doc)
 			for _, term := range terms{
-				if _, ok := vocabulary[term]; !ok {
-					for i := 0; i <= numOfClasses; i++{
-						vocabulary[term][i] = 0
+				 if  _, ok := vocabulary[term]; !ok {
+					for i := 0; i < numOfClasses; i++{
+					 vocabulary[term] = append(vocabulary[term], 0)
 					}
-				}else {
-					vocabulary[term][class] +=1
-				}
+				 }
+				  vocabulary[term][class] +=1
 			}
 		}
 	}
-
+ 
 	return vocabulary
-}
-
-
-//returns the confusion matrix which shows classification
-// accuracy by showing the correct and incorrect predictions on each class.
-func makeConfMatrix(testClassCorpus map[int] []string,
-				    numOfClasses int,
+ }
+ 
+ 
+ //returns the confusion matrix which shows classification
+ // accuracy by showing the correct and incorrect predictions on each class.
+ func makeConfMatrix(testClassCorpus map[int] []string,
+					numOfClasses int,
 					vocabulary map [string] []int,
 					sliceCondProb map [string] []float64, 
 					slicePriorC []float64 ) [][]int{
-
-	confusionMatrix := [][]int{}
+	 confusionMatrix := make([][]int, numOfClasses)
+	 for i := range confusionMatrix {
+		 confusionMatrix[i] = make([]int,numOfClasses)
+	 }
 	for classInd := 0; classInd <= numOfClasses;classInd++{
 		for _, doc := range testClassCorpus[classInd]{
 			classified_as_doc := applyMultinomialNB(sliceCondProb, slicePriorC,doc)
 			confusionMatrix[classInd][classified_as_doc] += 1
+			}
 		}
-	}
-
+	 fmt.Println(confusionMatrix)
 	return confusionMatrix
-}
-
-//returns sum of the elements of the matrix
-func sumMatrixValues(confusionMatrix [][]int) int{
+ }
+ 
+ //returns sum of the elements of the matrix
+ func sumMatrixValues(confusionMatrix [][]int) int{
 	var sum int
 	for _, col := range confusionMatrix{
 		for _, value := range col{
@@ -192,24 +190,24 @@ func sumMatrixValues(confusionMatrix [][]int) int{
 		}
 	}
 	return sum
-}
-
-//sum the values of slice of ints 
-func sum(sl []int) int {  
+ }
+ 
+ //sum the values of slice of ints 
+ func sum(sl []int) int {  
 	result := 0  
 	for _, numb := range sl {  
 	 result += numb  
 	}  
 	return result  
-}  
-
-func numberDocByClass(testClassCorpus map[int] []string) []int{
+ }  
+ 
+ func numberDocByClass(testClassCorpus map[int] []string) []int{
 	docsCountByClass := []int{}
-	for classInd, docs := range testClassCorpus{
-		docsCountByClass[classInd] = len(docs)
+	for _, docs := range testClassCorpus{
+		docsCountByClass = append(docsCountByClass, len(docs))
 	}
 	return docsCountByClass
-}
+ }
 
 //returns the Precision, F-score and the recall of the classificator for each document of a test set of documents
 func calcPRF(confusionMatrix [][]int, numOfClasses int, numAllDocsByClass []int) ([]float64, []float64,[] float64){
@@ -252,7 +250,3 @@ func calcOverall(precision []float64,recall  []float64, fScore []float64,
 	return precisionOverall, recallOverall, fScoreOverall
 }
 
-
-func main(){
-	
-}
--- project/main.go
+++ project/main.go
@@ -1,14 +1,264 @@
 package main
 
 import (
-	"encoding/csv"
+	//"encoding/csv"
 	"fmt"
-	"os"
-	"io"
 	"strings"
 	"strconv"
-
+	"io/ioutil"
+	// "path/filepath"
+    "log"
 )
+
+import 
+	"math"
+
+
+func convertText(document []string){}
+
+//function trains Multionomal Naive Bayes Classificator and returns vocabulary, priorC and cond probabilty
+func TrainMultinomialNB(classes map[int] []string) (map [string] []int,map [string] []float64, []float64 ){
+   numOfAllDocs := calNumAllDocs(classes)
+   numOfClasses := calNumOfClasses(classes)
+   vocabulary := makeVocabulary(classes,numOfClasses)
+   makeSliceOfNumDocsInClass := makeSliceOfNumDocsInClass(classes, numOfClasses)
+   slicePriorC := makeSlicePriorC(numOfAllDocs, makeSliceOfNumDocsInClass)
+   sliceTermCountClass := makeSliceTermCountInClass(numOfClasses, vocabulary)
+   sliceCondProb := makeSliceCondProb(vocabulary,sliceTermCountClass)
+   return vocabulary, sliceCondProb, slicePriorC
+}
+
+//returns the class corresponding to the text given by using formula 	
+func applyMultinomialNB(condProb map [string] []float64, priorC []float64, text string ) int{
+   
+   terms  := extractTerms(text)
+   var classificatedAs int 
+   var maxScore float64
+   fmt.Println(len(priorC))
+   for classInd, value := range priorC{
+	   score := math.Log(value)
+	   fmt.Println(classInd , score)
+	   for _, term := range terms{
+		   if condProbTerm, ok := condProb[term]; ok {
+			   score += math.Log(condProbTerm[classInd])
+		   }
+	   }
+	   //fmt.Println(classInd , score)
+	   if classInd == 0 || score > maxScore{
+		   maxScore = score
+		   classificatedAs = classInd
+	   }
+   }
+
+   return classificatedAs
+}
+
+//testing classifier accuracy
+func testClassifier(testClassCorpus map[int] []string, vocabulary map [string] []int, sliceCondProb map [string] []float64, slicePriorC []float64 ){
+   numOfClasses := len(testClassCorpus)
+   numDocsByClass := numberDocByClass(testClassCorpus)
+   confusionMatrix := makeConfMatrix(testClassCorpus, numOfClasses,vocabulary, sliceCondProb,slicePriorC)
+   precision, recall, fScore := calcPRF(confusionMatrix, numOfClasses, numDocsByClass)
+   precisionOverall, recallOverall,fScoreOverall := calcOverall(precision, recall, fScore, numOfClasses, numDocsByClass)
+   fmt.Println("Прецизност: ", precision)
+   fmt.Println("Обхват: ", recall)
+   fmt.Println("F-score: ", fScore)
+   fmt.Println("Обща прецизност: ", precisionOverall)
+   fmt.Println("Общ Обхват: ", recallOverall)
+   fmt.Println("Обща F-score: ", fScoreOverall)
+}
+//returns number of all docs in all classes
+func calNumAllDocs(classes map[int] []string) int{
+   numOfDocs := 0
+   for _,value := range classes{
+	   numOfDocs += len(value)
+   }
+   return numOfDocs
+}
+
+//returns number of classes 
+func calNumOfClasses(classes map[int] []string) int{
+   return len(classes)
+}
+
+//returns a slice of strings by spliting a given document into terms
+func extractTerms(doc string) []string{
+   return strings.Fields(doc)
+}
+
+//returns number of documents in each class
+func classDocsNum(classDocs []string) int{
+   return len(classDocs)
+}
+
+//returns a slice of number of documents in each class
+func makeSliceOfNumDocsInClass(classes map[int] []string, numOfClasses int) []int{
+   arrOfNumDocs := []int{}
+   for classInd := 0; classInd < len(classes);classInd++ {
+	   arrOfNumDocs = append(arrOfNumDocs, classDocsNum(classes[classInd]))
+   }
+   return arrOfNumDocs
+}
+
+//returns slice of probabilties of each class with the formula - 
+//count of documents in class divided by all documents in all classes
+func makeSlicePriorC(numOfAllDocs int, sliceOfNumDocsInClass []int) []float64{
+   arr := []float64{}
+
+   for _ ,docsCount := range sliceOfNumDocsInClass {
+	   arr = append(arr, float64(docsCount)/ float64(numOfAllDocs))
+   }
+   return arr
+}
+
+//returns a slice which each index matches the term in vocabulary of the same index 
+// and its value is the number of counts of the term in all documents 
+func makeSliceTermCountInClass(numOfClasses int,vocabulary map [string] []int) []int{
+   termCountArr := []int{}
+   for _, value := range vocabulary{
+	   for classInd:=0;classInd < numOfClasses; classInd++ {
+		   if len(termCountArr) >= numOfClasses{
+				termCountArr[classInd] += value[classInd] 
+		   }else{
+			   termCountArr = append(termCountArr,value[classInd])
+		   }
+	   }
+   }
+
+   return termCountArr
+}
+
+//the function receives as arguments a vocabulary and a slice with number of terms in each class
+//and returns map with key string and value slice of floats
+//the keys represent a term from vocabulary and the slice of floats has the values of the cond probability 
+//inside the innermost cycle is the the making of the slice which we assign to the every term of the vocabulary
+func makeSliceCondProb(vocabulary map [string] []int, sliceNumOfTermClass []int) map [string] []float64{
+   sliceCondProb := make(map [string] []float64)
+   temp := [] float64{}
+  // i := 0
+   sizeV := len(vocabulary)
+   for term, value := range vocabulary{
+	   for class,numOfTermsInClass := range sliceNumOfTermClass{
+		   temp = append(temp, (float64(value[class] + 1)/float64(numOfTermsInClass + sizeV)))		   
+	   }
+	   sliceCondProb[term] = temp
+	}
+   return sliceCondProb
+}
+
+//returns vocabulary of type map[string] []int where the key is a term 
+//and the slice contains for each class the term frequency
+func makeVocabulary(classes map[int] []string, numOfClasses int) map [string] []int{
+   vocabulary := make(map [string] []int)
+
+   for class, docs := range classes{
+	   for _, doc := range docs{
+		   terms := extractTerms(doc)
+		   for _, term := range terms{
+			    if  _, ok := vocabulary[term]; !ok {
+				   for i := 0; i < numOfClasses; i++{
+					vocabulary[term] = append(vocabulary[term], 0)
+				   }
+				}
+			     vocabulary[term][class] +=1
+		   }
+	   }
+   }
+
+   return vocabulary
+}
+
+
+//returns the confusion matrix which shows classification
+// accuracy by showing the correct and incorrect predictions on each class.
+func makeConfMatrix(testClassCorpus map[int] []string,
+				   numOfClasses int,
+				   vocabulary map [string] []int,
+				   sliceCondProb map [string] []float64, 
+				   slicePriorC []float64 ) [][]int{
+	confusionMatrix := make([][]int, numOfClasses)
+	for i := range confusionMatrix {
+		confusionMatrix[i] = make([]int,numOfClasses)
+	}
+   for classInd := 0; classInd <= numOfClasses;classInd++{
+	   for _, doc := range testClassCorpus[classInd]{
+		   classified_as_doc := applyMultinomialNB(sliceCondProb, slicePriorC,doc)
+		   confusionMatrix[classInd][classified_as_doc] += 1
+		   }
+	   }
+	fmt.Println(confusionMatrix)
+   return confusionMatrix
+}
+
+//returns sum of the elements of the matrix
+func sumMatrixValues(confusionMatrix [][]int) int{
+   var sum int
+   for _, col := range confusionMatrix{
+	   for _, value := range col{
+		   sum+= value
+	   }
+   }
+   return sum
+}
+
+//sum the values of slice of ints 
+func sum(sl []int) int {  
+   result := 0  
+   for _, numb := range sl {  
+	result += numb  
+   }  
+   return result  
+}  
+
+func numberDocByClass(testClassCorpus map[int] []string) []int{
+   docsCountByClass := []int{}
+   for _, docs := range testClassCorpus{
+	   docsCountByClass = append(docsCountByClass, len(docs))
+   }
+   return docsCountByClass
+}
+
+//returns the Precision, F-score and the recall of the classificator for each document of a test set of documents
+func calcPRF(confusionMatrix [][]int, numOfClasses int, numAllDocsByClass []int) ([]float64, []float64,[] float64){ 
+   
+   countDocsExtracted := sumMatrixValues(confusionMatrix)
+   precision := []float64{}
+   recall := []float64{}
+   fScore := []float64{}
+
+   for classInd := 0; classInd < numOfClasses; classInd++{
+	   if confusionMatrix[classInd][classInd] == 0{
+			precision = append(precision, 0.0)
+			recall = append(precision, 0.0)
+			fScore = append(precision, 0.0)
+	   }else{
+	   precision = append(precision, (float64(confusionMatrix[classInd][classInd]) / float64(countDocsExtracted)))
+	   recall    = append(recall,    (float64(confusionMatrix[classInd][classInd]) / float64(numAllDocsByClass[classInd])))
+	   fScore    = append(fScore,    ((2.0 * precision[classInd] * recall[classInd]) / (precision[classInd] + recall[classInd])))
+	   }
+   }
+   
+   return precision, recall, fScore
+}
+
+//returns overall Precision, Recall and F-score for every class 
+func calcOverall(precision []float64,recall  []float64, fScore []float64, 
+				numOfClasses int , countDocsClass[] int) (float64, float64, float64){
+
+   var precisionOverall float64 = 0.0
+   var recallOverall float64 = 0.0
+   var fScoreOverall float64 = 0.0
+   allDocs := sum(countDocsClass)
+
+   for classInd := 0; classInd < numOfClasses; classInd++{
+	   precisionOverall += (float64(precision[classInd] * precision[classInd])/ float64(allDocs))
+	   recallOverall    += (float64(countDocsClass[classInd]) * recall[classInd]) / float64(allDocs)
+   }
+
+   fScoreOverall += (2 * precisionOverall * recallOverall) / (precisionOverall + recallOverall)
+   return precisionOverall, recallOverall, fScoreOverall
+}
+
 
 func notCorruptedRecord(record []string) bool{
 	check := false
@@ -74,7 +324,7 @@ func change(classType string) int{
 	}
 	return 0
 }
-func putInClass(records [][]string){
+func putInClass(records [][]string)map[int] []string{
 
 	classes :=  make(map[int] []string)
 	for _, record := range records{
@@ -85,38 +335,88 @@ func putInClass(records [][]string){
 		classes[classType] = append(classes[classType], newRec)
 		
 	}
-	for key, _ := range classes{
-		fmt.Print(key)
-	}
+	return classes
 }
 
+//func main() {
+// 	csvFile, err := os.Open("D:\\FMI\\golang_workspace\\src\\golang_course\\project\\data1.csv")
+// 	defer csvFile.Close()
+
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println("Successfully Opened CSV file")
+//     csvLines := csv.NewReader(csvFile)
+// 	check := false
+// 	var str1 [][]string
+// 	for {
+		
+// 		record, err1 := csvLines.Read()
+// 		if(err1 == io.EOF){
+// 			break
+// 		}
+		
+// 		if notCorruptedRecord(record){
+// 			if check == true{
+// 			record = record[2:len(record)-1]
+// 			record = convertFloatToStrRecord(record) 
+// 			str1 = append(str1, record)
+// 			}
+// 		}
+// 		check = true
+// 	 }
+// 	//fmt.Println(str1)
+// 	classes := putInClass(str1)
+// 	testSet := make(map[int] []string)
+// 	trainSet := make(map[int] []string)
+// 	for class, docs:= range classes{
+// 		fmt.Println(len(docs))
+// 		testCount := float64(len(docs)) * 0.1
+// 		testSet[class] = docs[:int(testCount)]
+// 		trainSet[class] = docs[int(testCount):]
+// 	}
+// 	vocabulary, condProb, prior := TrainMultinomialNB(trainSet)
+// 	fmt.Println(applyMultinomialNB(condProb, prior,"i didn't know humanity back love and family"))
+// 	testClassifier(testSet, vocabulary,condProb, prior)
+// }
+
+func convert(record string) string{
+	return strings.Replace(record, "***", "", -1)
+}
 func main() {
-	csvFile, err := os.Open("D://FMI//golang_workspace//src//project//data1.csv")
-	defer csvFile.Close()
-
+	classes := make(map [int] []string)
+	filesAll, err := ioutil.ReadDir("src\\golang_course\\project\\all")
 	if err != nil {
-		fmt.Println(err)
+        log.Fatal(err)
 	}
-	fmt.Println("Successfully Opened CSV file")
-    csvLines := csv.NewReader(csvFile)
-	check := false
-	var str1 [][]string
-	for {
-		
-		record, err1 := csvLines.Read()
-		if(err1 == io.EOF){
-			break
-		}
-		
-		if notCorruptedRecord(record){
-			if check == true{
-			record = record[2:len(record)-1]
-			record = convertFloatToStrRecord(record) 
-			str1 = append(str1, record)
+	for i := 0; i < len(filesAll) - 1; i++{
+		for _, classF := range filesAll {
+			classNames := "src\\golang_course\\project\\all\\" + classF.Name()
+			files, err := ioutil.ReadDir(classNames)
+			if err != nil {
+				log.Fatal(err)
 			}
+			records := []string{}
+			for _, f := range files {
+				fileName := "src\\golang_course\\project\\all\\C-Culture\\" + f.Name()
+				record,_ := ioutil.ReadFile(fileName)
+				recordStr := convert(string(record))
+			//	fmt.Println(recordStr)
+				records = append(records, recordStr)
+			}
+			classes[i] = records
 		}
-		check = true
-	 }
-	putInClass(str1)
-
+	}
+	testSet := make(map[int] []string)
+	trainSet := make(map[int] []string)
+	for class, docs:= range classes{
+		testCount := float64(len(docs)) * 0.1
+		testSet[class] = docs[:int(testCount)]
+		trainSet[class] = docs[int(testCount):]
+	}
+	_, condProb, prior := TrainMultinomialNB(trainSet)
+	text1 := "Ние виждаме една серия от управленски провали. Укрепено ли е правителството - не то не съществува. равителството е несъществуващото - в този тежък момент, когато достигаме близо 1000 заразени на ден, ако говорим за здравния проблем "
+	fmt.Println(applyMultinomialNB(condProb, prior, text1 ))
+	//testClassifier(testSet, vocabulary,condProb, prior)
 }
+	
