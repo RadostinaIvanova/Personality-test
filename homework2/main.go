71442e04494170b05055d1e2e2c3bc46dca5529c
RadostinaIvanova <radi.e.ivanova@gmail.com>
2021-01-18 23:45:33 +0200

    homework2 task2

--- homework2/main.go
+++ homework2/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import ( "fmt"
+		 "errors"
+		 "time"
+)
 type Task interface {
     Execute(int) (int, error)
 }
@@ -15,8 +18,11 @@ func Pipeline(tasks ...Task) operationsConsecutive{
 
 func (ops operationsConsecutive) Execute(addEnd int) (int, error){
 	var err error
-	for _, value := range ops.tasks{
-		addEnd, err = value.Execute(addEnd)
+	if len(ops.tasks) == 0 {
+		return 0, errors.New("No tasks given")
+	}
+	for _, task := range ops.tasks{
+		addEnd, err = task.Execute(addEnd)
 		if err != nil{
 			return addEnd, err
 		}
@@ -29,9 +35,33 @@ type operationsConcurrent struct{
 }
 
 
+func Fastest(tasks ...Task) operationsConcurrent{
+	return operationsConcurrent{tasks}
+}
+
+func (ops operationsConcurrent) Execute(addEnd int) (int, error){
+	numTasks := len(ops.tasks)
+	out := make(chan int, numTasks )
+    errs := make(chan error, numTasks)
+	if len(ops.tasks) == 0 {
+		return 0, errors.New("No tasks given")
+	}
+	for _, task :=  range ops.tasks{
+		go func() {
+			taskRes ,err := task.Execute(addEnd)
+			errs <- err
+		    out <- taskRes
+			 }()
+	}	
+	res := <- out
+	err := <- errs
+	return res, err
+}
+
 type adder struct {
 	augend int
 }
+
 
 func (a adder) Execute(addend int) (int, error) {
 	result := a.augend + addend
@@ -40,14 +70,24 @@ func (a adder) Execute(addend int) (int, error) {
 	}
 	return result, nil
 }
-//func Pipeline(tasks ...Task){
 
-//}
+type lazyAdder struct {
+	adder
+	delay time.Duration
+}
+
+func (la lazyAdder) Execute(addend int) (int, error) {
+	time.Sleep(la.delay * time.Millisecond)
+	return la.adder.Execute(addend)
+}
+
 func main(){
 	if res, err := 	Pipeline(adder{20}, adder{10}, adder{-50}).Execute(100); err != nil {
 		fmt.Printf("The pipeline returned an error\n")
 	} else {
 		fmt.Printf("The pipeline returned %d\n", res)
 	}
+	f := Fastest()
+	fmt.Println(f.Execute(1))
 
 }
